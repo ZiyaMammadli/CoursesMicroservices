a358e068689487c1c721f28f196129f93b8a53b4
Your Name <you@example.com>
2025-04-09 16:10:45 +0400

    first commit

---
+++ src/services/catalog/CoursesMicroservice.Catalog.Api/Features/Categories/Category.cs
@@ -0,0 +1,11 @@
+﻿using CoursesMicroservice.Catalog.Api.Features.Courses;
+using CoursesMicroservice.Catalog.Api.Repositories;
+
+namespace CoursesMicroservice.Catalog.Api.Features.Categories
+{
+    public class Category:BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public List<Course>? Courses { get; set; }
+    }
+}
---
+++ src/services/catalog/CoursesMicroservice.Catalog.Api/Features/Courses/Course.cs
@@ -0,0 +1,17 @@
+﻿using CoursesMicroservice.Catalog.Api.Features.Categories;
+using CoursesMicroservice.Catalog.Api.Repositories;
+
+namespace CoursesMicroservice.Catalog.Api.Features.Courses
+{
+    public class Course:BaseEntity
+    {
+        public Guid UserId { get; set; } = default!;
+        public Guid CategoryId { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; } = default!;
+        public string? Picture { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Category Category { get; set; } = default!;
+    }
+}
