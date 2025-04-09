d5fcdb93dd6aa9acda8350946a599fba593bf5f3
Your Name <you@example.com>
2025-04-09 17:14:14 +0400

    createdCategoryConfiguration

--- src/services/catalog/CoursesMicroservice.Catalog.Api/Repositories/AppDbContext.cs
+++ src/services/catalog/CoursesMicroservice.Catalog.Api/Repositories/AppDbContext.cs
@@ -8,6 +8,11 @@ namespace CoursesMicroservice.Catalog.Api.Repositories
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CourseConfiguration).Assembly);
+        }
+
         DbSet<Course> Courses { get; set; }
         DbSet<Category> Categories { get; set; }
     }
---
+++ src/services/catalog/CoursesMicroservice.Catalog.Api/Repositories/CategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CoursesMicroservice.Catalog.Api.Features.Categories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace CoursesMicroservice.Catalog.Api.Repositories
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToCollection("categories");
+            builder.HasKey(x => x.Id);
+            builder.Property(c=>c.Id).ValueGeneratedNever();
+            builder.Property(c => c.Name).HasElementName("name").HasMaxLength(100);
+            builder.Ignore(c => c.Courses);
+        }
+    }
+}
